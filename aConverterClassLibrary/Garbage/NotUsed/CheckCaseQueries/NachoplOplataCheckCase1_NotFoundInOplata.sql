6fffd9b2a172d1b08ab011f494e36877343b2bcd
asv765 <asv765@mail.ru>
2015-08-06 16:24:50 +0300

    Наведен порядок - удалено все лишнее

--- aConverterClassLibrary/Garbage/NotUsed/CheckCaseQueries/NachoplOplataCheckCase1_NotFoundInOplata.sql
+++
@@ -1,7 +0,0 @@
-﻿select COUNT(*) from cnv$nachopl n
-where n.oplata <> 0 and not exists
-      (select lshet from cnv$oplata o
-       where n.lshet = o.lshet and
-             n.servicecd = o.servicecd and
-             n.year_ = o.year_ and
-             n.month_ = o.month_)
--- aConverterClassLibrary/Garbage/NotUsed/StatisticQueries/NachoplOplataSummaMismatchErrorStatistic.sql
+++
@@ -1,8 +0,0 @@
-﻿select n.lshet, n.servicecd, n.servicenam, n.month_, n.year_, n.oplata, SUM(o.summa) as summa
-from cnv$nachopl n inner join cnv$oplata o
-    on n.lshet = o.lshet and
-    n.servicecd = o.servicecd and
-    n.year_ = EXTRACT(YEAR FROM o.date_vv) and
-    n.month_ = EXTRACT(MONTH FROM o.date_vv)
-group BY n.lshet, n.servicecd, n.servicenam, n.month_, n.year_, n.oplata
-HAVING n.oplata <> SUM(o.summa)
